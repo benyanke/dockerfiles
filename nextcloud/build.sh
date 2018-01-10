f9d96c60ece5b24ba583af8a7ea3ae9f3a8cacd6
Ben Yanke <ben@benyanke.com>
2018-01-10 09:13:34 -0600

    Update build.sh

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -9,10 +9,14 @@
   docker build --force-rm -f Dockerfile.12.0 -t benyanke/nextcloud:12.0 .;
   docker push benyanke/nextcloud:12.0;
 ) &
+(
+  docker build --force-rm -f Dockerfile.12.0 -t benyanke/nextcloud:latest .;
+  docker push benyanke/nextcloud:latest;
+) &
 
 (
-  docker build --force-rm -f Dockerfile.daily -t benyanke/nextcloud:latest .;
-  docker push benyanke/nextcloud:latest;
+  docker build --force-rm -f Dockerfile.daily -t benyanke/nextcloud:daily .;
+  docker push benyanke/nextcloud:daily;
 ) &
 
 wait
