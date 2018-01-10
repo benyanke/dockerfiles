38ee53a5df7e5e9b545b34df72c1e0e4d4afade1
do.benyanke.com Server <noreply@do.benyanke.com>
2018-01-10 05:07:50 +0000

    Prepping nextcloud images to build under new ownership

--- nextcloud/build.sh
+++ nextcloud/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-docker build --force-rm -f Dockerfile.11.0 -t benyanke/nextcloud:11.0 .;
-docker build --force-rm -f Dockerfile.12.0 -t benyanke/nextcloud:12.0 .;
-docker build --force-rm -f Dockerfile.daily -t benyanke/nextcloud:latest .;
-docker push benyanke/nextcloud:latest;
-docker push benyanke/nextcloud:11.0;
-docker push benyanke/nextcloud:12.0;
+(
+  docker build --force-rm -f Dockerfile.11.0 -t benyanke/nextcloud:11.0 .;
+  docker push benyanke/nextcloud:11.0;
+) &
+
+(
+  docker build --force-rm -f Dockerfile.12.0 -t benyanke/nextcloud:12.0 .;
+  docker push benyanke/nextcloud:12.0;
+) &
+
+(
+  docker build --force-rm -f Dockerfile.daily -t benyanke/nextcloud:latest .;
+  docker push benyanke/nextcloud:latest;
+) &
+
+wait
+echo "Build and push complete"
