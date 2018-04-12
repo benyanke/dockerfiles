28c423d8701c5dc41c281deb9705fb7ac17634d4
Ben Yanke <ben@benyanke.com>
2018-04-11 21:06:11 -0500

    Add nightly build

--- glauth/build.sh
+++ glauth/build.sh
@@ -3,5 +3,8 @@
   docker build --force-rm -f Dockerfile -t benyanke/glauth:latest .;
   docker push benyanke/glauth:latest;
 
+  docker build --force-rm -f Dockerfile.dev -t benyanke/glauth:nightly .;
+  docker push benyanke/glauth:nightly;
+
 # wait
 echo "Build and push complete"
